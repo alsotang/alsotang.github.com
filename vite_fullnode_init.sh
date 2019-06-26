ae8fd38170a73a271216b363207ff801eef19b57
alsotang <alsotang@gmail.com>
2019-06-26 11:03:46 +0800

    go-vite v2.2.0

--- vite_fullnode_init.sh
+++ vite_fullnode_init.sh
@@ -1,6 +1,6 @@
 ## Before run this script, you should define $VITE_ADDR outside. Otherwise you would not receive fullnode rewards.
 
-GVITE_VERSION=v2.1.4
+GVITE_VERSION=v2.2.0
 VITE_DIR=$HOME/gvite
 ETH0_IP=`/sbin/ifconfig eth0 | grep 'inet' | awk '{print $2}' | head -n1`
 
